a1add01f1f2958271bb4f6db9f63fb96c9232b60
objetive218 <davidsantiagotc@gmail.com>
2024-08-28 18:25:11 -0500

    reorganizing structure

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -3,11 +3,15 @@ const Movie = ({data, typeStyle}) => {
     const typeData = data.type === "movie" ? "./movie_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg" : "./tv_gen_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg";
     return (
        <>
-            <h1 style={{color:"blue"}}>{data.title}</h1>
-            <p>{data.year}</p>
-            <p><img src={typeData} alt="type content" /> {data.type}</p>
-            <img src={data.image} alt="content banner" />
-            <img src="./bookmark_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg" alt="bookMark" />
+            <img src={data.image} alt="content banner"  className="bannerMovie"/>
+            <div className="infoMovie">
+            <p className="yearMovie">{data.year}</p>
+            
+            <img src={typeData} alt="type content" />
+             <p>{data.type}</p>
+            </div>
+            <h1 className="titleMovie" >{data.title}</h1>
+            <img src="./bookmark_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg" alt="bookMark"  className="bookMarkMovie"/>
        </>
     );  
 }
