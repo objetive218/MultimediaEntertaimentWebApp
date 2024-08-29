16518f47b2276d1a11868d95d199d22353b2991c
objetive218 <davidsantiagotc@gmail.com>
2024-08-29 16:06:03 -0500

    change secction to main and add classnames

--- src/App.jsx
+++ src/App.jsx
@@ -25,9 +25,9 @@ function App() {
   }
 
   return (
-    <section>
+    <main>
       <NavBar/>
-      <form action="#">
+      <form action="#" className='searchForm'>
         <img src="./search_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg" alt="lupa" />
         <input type="text" value={search} onChange={handleSearch} placeholder='Search for movies or TV series'/>
       </form>
@@ -42,7 +42,7 @@ function App() {
           ) 
         }
         </ul>
-        </section>
+        </main>
   )
 }
 
