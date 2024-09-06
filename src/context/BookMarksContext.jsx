52ba3a2b91dd1f232c4240a2f62a0218298c59f6
objetive218 <davidsantiagotc@gmail.com>
2024-09-06 14:54:15 -0500

    create a context for bookmarks

--- src/context/BookMarksContext.jsx
+++ src/context/BookMarksContext.jsx
@@ -3,18 +3,23 @@ import { createContext, useState } from "react";
 const BookMarksContext = createContext(null);
 const BookProvider = function ({ children }) {
   const [bookMarks, setBookMarks] = useState([{
-            "Title": "The Lord of the Rings: The Fellowship of the Ring",
-            "Year": "2001",
-            "imdbID": "tt0120737",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg"
+            id: "tt0120737",
+            title: "The Lord of the Rings: The Fellowship of the Ring",
+            year: "2001",
+            image: "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg",
+            type: "movie",
         }]);
   const handleBookMarks = (e) => {
-    if(bookMarks.find()){}
+    if(bookMarks.includes(e))
+      return 
+    
+    else{
+      setBookMarks([...bookMarks, e])
+    }
   }
 
   return (
-    <BookMarksContext.Provider value={{bookMarks, setBookMarks}}>
+    <BookMarksContext.Provider value={{bookMarks, setBookMarks, handleBookMarks}}>
       {children}
     </BookMarksContext.Provider>
   );
