b6a51b10c70e0fc24599b9607868a29386f34c33
objetive218 <davidsantiagotc@gmail.com>
2024-09-12 16:26:19 -0500

    create state to manage form visibility and toggle login and create user

--- src/App.jsx
+++ src/App.jsx
@@ -12,13 +12,19 @@ import BookMarks from './components/BookMarks';
 import Series from './components/Series';
 import SearchResults from './components/SearchResults';
 import CreateUser from './components/CreateUser';
+import Login from './components/Login';
 
 
 function App() {
   const {search, setSearch, error} = useSearch();
   const {movies, getMovies} = useMovies({search})
   const [actualPagestate, setActualPagestate] = useState("home");
+  const [loginView, setLoginView]= useState({step: false, visibility: false});
 
+  const handleLoginView = () => {
+    if(loginView.step){setLoginView({ step:false, visibility: false})}
+    else{setLoginView({step:true, visibility: false})}
+  }
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const debouncedGetMovies = useCallback(debounce(search => {
     getMovies(search);
@@ -33,7 +39,7 @@ function App() {
   
   return (
     <main>
-      <NavBar page={actualPagestate} changePage={setActualPagestate} setSearch={setSearch}/>
+      <NavBar page={actualPagestate} changePage={setActualPagestate} setSearch={setSearch} handleLoginView={handleLoginView}/>
       <Search search={search} handleSearch={handleSearch}/>
       {actualPagestate === "home" && search.length < 3 && <Trending/>}
       {actualPagestate === "home" && search.length < 3 && <Recommend/>}
@@ -41,7 +47,13 @@ function App() {
       {actualPagestate === "series" && search.length < 3 && <Series/>}
       {actualPagestate === "bookMarks" && search.length < 3 && <BookMarks />}
       {search.length > 3 && <SearchResults movies={movies} search={search}/>}
-      <CreateUser/>
+      {loginView.step === true &&
+      <>
+      {loginView.visibility === false && <Login step={loginView} setStep={setLoginView} />}
+      {loginView.visibility === true && <CreateUser step={loginView} setStep={setLoginView}/>}
+      </> 
+      }
+      
     </main>
   )
 }
